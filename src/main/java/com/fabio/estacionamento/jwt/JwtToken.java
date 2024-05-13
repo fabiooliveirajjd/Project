f2e34a7a963d01055da1983cce0b8b217ff791ae
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-13 15:27:40 -0300

    adicionando atributo token na classe JwtToken

--- src/main/java/com/fabio/estacionamento/jwt/JwtToken.java
+++ src/main/java/com/fabio/estacionamento/jwt/JwtToken.java
@@ -1,4 +1,12 @@
 package com.fabio.estacionamento.jwt;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class JwtToken {
+    private String token;
 }
