934dc8286cece75519d4967edc80d82898b9c9a4
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-13 15:53:59 -0300

    comentários JwtUtils

--- src/main/java/com/fabio/estacionamento/jwt/JwtUtils.java
+++ src/main/java/com/fabio/estacionamento/jwt/JwtUtils.java
@@ -4,12 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class JwtUtils {
 
-    public static final String JWT_BEARER = "Bearer ";
-    public static final String JWT_AUTHORIZATION = "Authorization";
-    public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
-    public static final long EXPIRE_DAYS = 0;
-    public static final long EXPIRE_HOURS = 0;
-    public static final long EXPIRE_MINUTES = 2;
+    public static final String JWT_BEARER = "Bearer "; // Bearer é o tipo de autenticação
+    public static final String JWT_AUTHORIZATION = "Authorization"; // Authorization é o cabeçalho da requisição
+    public static final String SECRET_KEY = "0123456789-0123456789-0123456789"; // chave secreta para criptografia
+    public static final long EXPIRE_DAYS = 0; // tempo de expiração do token em dias
+    public static final long EXPIRE_HOURS = 0; // tempo de expiração do token em horas
+    public static final long EXPIRE_MINUTES = 2; // tempo de expiração do token em minutos
 
     private JwtUtils(){
     }
