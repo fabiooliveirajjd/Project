0fd1a8041d2dc9e0bac9647db27d06704837f5ca
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-13 15:24:12 -0300

    Criação da método passwordEncoder e authenticationManager

--- src/main/java/com/fabio/estacionamento/config/SpringSecurityConfig.java
+++ src/main/java/com/fabio/estacionamento/config/SpringSecurityConfig.java
@@ -3,9 +3,13 @@ package com.fabio.estacionamento.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -28,4 +32,13 @@ public class SpringSecurityConfig {
                 ).build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder (){
+        return new BCryptPasswordEncoder(); // Método que criptografa a senha
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }
