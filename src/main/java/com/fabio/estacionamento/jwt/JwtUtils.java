d0e1cb1acc6a89d17bc3bf53ff85392f120dec5b
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-14 09:31:37 -0300

    cm©todo que faz o cáuculo entre a data que o token foi criado e deve expirar

--- src/main/java/com/fabio/estacionamento/jwt/JwtUtils.java
+++ src/main/java/com/fabio/estacionamento/jwt/JwtUtils.java
@@ -3,7 +3,11 @@ package com.fabio.estacionamento.jwt;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.slf4j.Slf4j;
 
+import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
 
 @Slf4j
 public class JwtUtils {
@@ -19,11 +23,18 @@ public class JwtUtils {
     public static final long EXPIRE_MINUTES = 2; // tempo de expiração do token em minutos
 
     //construtor privado para garantir que a classe não seja instanciada
-    private JwtUtils(){
+    private JwtUtils() {
     }
 
-    // método que retorna a chave secreta
-    private static Key getSecretKey(){
-        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes());
+    // método que prepara a chave secreta para a criptografia
+    private static Key getSecretKey() {
+        return Keys.hmacShaKeyFor(SECRET_KEY.getBytes(StandardCharsets.UTF_8)); // retorna a chave secreta
+    }
+
+    //método que faz o cáuculo entre a data que o token foi criado e a data que deve ser expirado
+    private static Date toExpireDate (Date start) {
+        LocalDateTime dateTime = start.toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime(); // converte a data para LocalDateTime definidas na config
+        LocalDateTime end = dateTime.plusDays(EXPIRE_DAYS).plusHours(EXPIRE_HOURS).plusMinutes(EXPIRE_MINUTES); // adiciona os dias, horas e minutos
+        return Date.from(end.atZone(ZoneId.systemDefault()).toInstant()); // retorna a data final
     }
 }
