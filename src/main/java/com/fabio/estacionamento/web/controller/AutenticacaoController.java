4614a5a1700f4a05d94227859ea9808f50973026
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-15 11:36:57 -0300

    Classe JwtAuthorizationFilter que extende de OncePerRequestFilter

--- src/main/java/com/fabio/estacionamento/web/controller/AutenticacaoController.java
+++ src/main/java/com/fabio/estacionamento/web/controller/AutenticacaoController.java
@@ -13,6 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -33,6 +34,7 @@ public class AutenticacaoController {
      *  se existir retorna um objeto authenticationToken e adicona esse objeto como parte do contexto do spring security
      *  pelo método autenticatede, caso contrário, retorna uma exceção de autenticação, se passar, cai no bloco try e cria o token.
      */
+    @PostMapping("/auth")
     public ResponseEntity<?> autenticar (@RequestBody @Valid UsuarioLoginDto dto, HttpServletRequest request) {
         log.info("processo de autenticação pelo login {}", dto.getUsername());
         try{
