d475d3123ef11dfb095741021dd1839745053218
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-15 14:17:00 -0300

    Criptografando senha do usuário ao criar no Postman

--- src/main/java/com/fabio/estacionamento/service/UsuarioService.java
+++ src/main/java/com/fabio/estacionamento/service/UsuarioService.java
@@ -6,6 +6,7 @@ import com.fabio.estacionamento.exception.PasswordInvalidException;
 import com.fabio.estacionamento.exception.UsernameUniqueViolationException;
 import com.fabio.estacionamento.repository.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,10 +17,12 @@ import java.util.List;
 public class UsuarioService {
 
     private final UsuarioRepository usuarioRepository;
+    private PasswordEncoder passwordEncoder;
 
     @Transactional // essa anotação indica que o método irá realizar operações de escrita no banco de dados.
     public Usuario salvar(Usuario usuario) {
         try {
+            usuario.setPassword(passwordEncoder.encode(usuario.getPassword()));
             return usuarioRepository.save(usuario);
         } catch (org.springframework.dao.DataIntegrityViolationException ex) {
             throw new UsernameUniqueViolationException(String.format("Username '%s' já cadastrado", usuario.getUsername()));
