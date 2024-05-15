53ea7deec44f6cf47befe3a44f1fab256e168e4c
Fabio Oliveira <fabio.oliveira.prestador@prf.gov.br>
2024-05-15 11:01:33 -0300

    UsuarioLoginDto criado

--- src/main/java/com/fabio/estacionamento/web/controller/AutenticacaoController.java
+++ src/main/java/com/fabio/estacionamento/web/controller/AutenticacaoController.java
@@ -1,8 +1,11 @@
 package com.fabio.estacionamento.web.controller;
 
+import com.fabio.estacionamento.jwt.JwtToken;
 import com.fabio.estacionamento.jwt.JwtUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     private final JwtUserDetailsService detailsService;
+    private final AuthenticationManager authenticationManager;
+
+    public ResponseEntity<JwtToken> autenticar ()
 }
 
---
+++ src/main/java/com/fabio/estacionamento/web/dto/UsuarioLoginDto.java
@@ -0,0 +1,23 @@
+package com.fabio.estacionamento.web.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UsuarioLoginDto {
+
+    @NotBlank
+    @Email(message = "formato do e-mail está invalido", regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private String username;
+    @NotBlank
+    @Size(min = 6, max = 6)
+    private String password;
+}
